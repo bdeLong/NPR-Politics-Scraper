3461be1d309f62bef5d7f3c76b16b9c7788b4cf7
Ben deLong <bendelong45@gmail.com>
2019-01-30 00:16:40 -0600

    changes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-// Grab the articles as a json
 $.getJSON("/scrape");
 $("#articles").hide();
 
@@ -16,67 +15,71 @@ $(document).on("click", "#scrape-button", function () {
 
 
 $(document).on("click", ".view-comments", function () {
-  // Empty the notes from the note section
   $("#notes").empty();
-  // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
-  // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
     url: `/articles/${thisId}`
   })
-    // With that done, add the note information to the page
     .then(function (data) {
-
-      // The title of the article
       $("#notes").append(`<h5>${data.title}</h5>`);
-      $("#notes").append(`<h4>Comments:`)
+      $("#notes").append(`<h4>Comments:<br><br>`)
       if (data.note.length > 0) {
         data.note.forEach(note => {
-          $("#notes").append(`<h5>${note.title} says: ${note.body}</h5>`)
+          $("#notes").append(`<h5>${note.title} says: ${note.body}</h5><br>`)
         })
       }
       else {
-        $("#notes").append(`<h5>Be the first to leave a comment!</h5>`)
+        $("#notes").append(`<h5>Be the first to leave a comment!</h5><br>`)
       }
-      // An input to enter a new title
+      $("#notes").append("<br><h4>Leave a comment</h4>")
       $("#notes").append("<input id='titleinput' name='title' placeholder='Name'>");
-      // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Comment'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
       $("#notes").append(`<button data-id='${data._id}' id='savenote'>Save Note</button>`);
     });
 });
 
-// When you click the savenote button
 $(document).on("click", "#savenote", function () {
-  // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: `/articles/${thisId}`,
     data: {
-      // Value taken from title input
       title: $("#titleinput").val(),
-      // Value taken from note textarea
       body: $("#bodyinput").val()
     }
   }).then(function () {
     $.ajax({
       method: "GET",
       url: `/articles/${thisId}`
-    }).then(function (data) {
-      $("#notes h5, h6").empty();
-      data.note.forEach(note => {
-        $("#notes").append(`<h5>Name: ${note.title}</h5> <h6>Comment: ${note.body}</h6>`)
-      })
     })
+      .then(function (data) {
+        $("#notes").append(`<h5>${data.title}</h5>`);
+        $("#notes").append(`<h4>Comments:<br><br>`)
+        if (data.note.length > 0) {
+          data.note.forEach(note => {
+            $("#notes").append(`<h5>${note.title} says: ${note.body}</h5> <br>`)
+          })
+        }
+        else {
+          $("#notes").append(`<h5>Be the first to leave a comment!</h5> <br>`)
+        }
+        $("#notes").append("<br><h4>Leave a comment</h4>")
+        $("#notes").append("<input id='titleinput' name='title' placeholder='Name'>");
+        $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Comment'></textarea>");
+        $("#notes").append(`<button data-id='${data._id}' id='savenote'>Save Note</button>`);
+      });
+    // $.ajax({
+    //   method: "GET",
+    //   url: `/articles/${thisId}`
+    // }).then(function (data) {
+    //   $("#notes h5, h6").empty();
+    //   data.note.forEach(note => {
+    //     $("#notes").append(`<h5>Name: ${note.title}</h5> <h6>Comment: ${note.body}</h6>`)
+    //   })
+    // })
   })
-
-  // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
--- server.js
+++ server.js
@@ -6,34 +6,22 @@ const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
-// Initialize Express
 const app = express();
-
-// Configure middleware
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
 mongoose.connect("mongodb://bdelong:mongodb45@ds155411.mlab.com:55411/heroku_s19w3vgl", { useNewUrlParser: true });
 
 // Routes
 
-// A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
-  // First, we grab the body of the html with axios
   axios.get("https://www.npr.org/sections/politics/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
     $(".item-info").each(function (i, element) {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children(".title")
         .children("a")
@@ -47,61 +35,48 @@ app.get("/scrape", function (req, res) {
         .children("a")
         .text();
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.update(result, result, { upsert: true })
+      db.Article.updateMany(result, result, { upsert: true })
         .then(function (dbArticle) {
-          // View the added result in the console
         })
         .catch(function (err) {
-          // If an error occurred, log it
           if (err) { throw err };
         });
     });
   });
 });
 
-// Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
-  // Grab every document in the Articles collection
+
   db.Article.find({})
     .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function (err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .populate("note")
     .then(function (dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function (err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
   db.Note.create(req.body)
     .then(function (dbNote) {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id, $position: 0 } });
     })
     .then(function (dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function (err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
